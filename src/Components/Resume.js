b69deeab18ccaeb4e8fc3486bf4bad737827fe10
Skiboybl <69096827+Skiboybl@users.noreply.github.com>
2020-09-23 15:21:51 +0100

    stable to load

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -6,9 +6,7 @@ class Resume extends Component {
     if(this.props.data){
       var skillmessage = this.props.data.skillmessage;
       var education = this.props.data.education.map(function(education){
-        return <div key={education.school}><h3>{education.school}</h3>
-        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
+        return 
       })
       var work = this.props.data.work.map(function(work){
         return <div key={work.company}><h3>{work.company}</h3>
@@ -27,7 +25,7 @@ class Resume extends Component {
 
       <div className="row education">
          <div className="three columns header-col">
-            <h1><span>Education</span></h1>
+         
          </div>
 
          <div className="nine columns main-col">
