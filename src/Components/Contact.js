8ea8f790b464bd0c33ef7449f704aadf481014c1
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-06-06 10:45:31 +0100

    CUSTOM Portfolio

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,100 +1,144 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
+import { init } from "emailjs-com";
+
+init("user_W7s5J5ipgDjWdY4yR0NLs");
 
 const Contact = ({ data }) => {
-   const [url, setUrl] = useState('mailto:test@example.com?subject=subject&body=body');
-   const [name, setName] = useState('');
-   const [subject, setSubject] = useState('');
-   const [email, setEmail] = useState('');
-   const [message, setMessage] = useState('');
+  const [url, setUrl] = useState(
+    "mailto:test@example.com?subject=subject&body=body"
+  );
+  const [name, setName] = useState("");
+  const [subject, setSubject] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-   console.log(data)
+  const handleClick = (e) => {
+    e.preventDefault();
+    window.open(`mailto:${email}?subject=${subject}&body=${name}: ${message}`);
+  };
 
-    const handleClick = (e) => {
-       e.preventDefault();
-      window.open(`mailto:${email}?subject=${subject}&body=${name}: ${message}`);
-    }
-    
+  function sendEmail(e) {
+    e.preventDefault();
 
-    return (
-      <section id="contact">
+    emailjs
+      .sendForm(
+        "gmail",
+        "template_gzl3w69",
+        e.target,
+        "user_W7s5J5ipgDjWdY4yR0NLs"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  }
 
-         <div className="row section-head">
+  return (
+    <section id="contact">
+      <div className="row section-head">
+        <div className="two columns header-col">
+          <h1>
+            <span>Get In Touch.</span>
+          </h1>
+        </div>
 
-            <div className="two columns header-col">
-
-               <h1><span>Get In Touch.</span></h1>
-
-            </div>
-
-            <div className="ten columns">
-
-                  <p className="lead">{data?.message}</p>
-
-            </div>
-
-         </div>
-
-         <div className="row">
-            <div className="eight columns">
-
-               <form id="contactForm" name="contactForm">
-					<fieldset>
-
-                  <div>
-						   <label htmlFor="contactName">Name <span className="required">*</span></label>
-						   <input value={name} type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={e => setName(e.target.value)}/>
-                  </div>
-
-                  <div>
-						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input value={email} type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={e=> setEmail('codexpath3@gmail.com')}/>
-                  </div>
-
-                  <div>
-						   <label htmlFor="contactSubject">Subject</label>
-						   <input value={subject} type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={e => setSubject(e.target.value)}/>
-                  </div>
-
-                  <div>
-                     <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea value={message} onChange={e => setMessage(e.target.value)} cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
-                  </div>
-
-                  <div>
-                     <button type='submit' onClick={handleClick} className="submit">Submit</button>
-                     <span id="image-loader">
-                        <img alt="" src="images/loader.gif" />
-                     </span>
-                  </div>
-					</fieldset>
-				   </form>
-
-           <div id="message-warning"> not sent try again</div>
-				   <div id="message-success">
-                  <i className="fa fa-check"></i>Your message was sent, thank you!<br />
-				   </div>
-           </div>
-
-
-            <aside className="four columns footer-widgets">
-               <div className="widget widget_contact">
-
-					   <h4>Address and Phone</h4>
-					   <p className="address">
-						   {data?.name}<br />
-						   {data?.address.street} <br />
-						   {data?.address.city}, {data?.address.state} {data?.address.zip}<br />
-						   <span>{data?.phone}</span>
-					   </p>
-				   </div>
-
-               <div className="widget widget_tweets">
-
-		         </div>
-            </aside>
+        <div className="ten columns">
+          <p className="lead">{data?.message}</p>
+        </div>
       </div>
-   </section>
-    );
-}
+
+      <div className="row">
+        <div className="eight columns">
+          <form id="contactForm" name="contactForm">
+            <fieldset>
+              <div>
+                <label>
+                  Name <span className="required">*</span>
+                </label>
+                <input
+                  type="text"
+                  size="35"
+                  id="contactName"
+                  name="contactName"
+                />
+              </div>
+
+              <div>
+                <label>
+                  Email <span className="required">*</span>
+                </label>
+                <input
+                  type="text"
+                  size="35"
+                  id="contactEmail"
+                  name="contactEmail"
+                />
+              </div>
+
+              <div>
+                <label>Subject</label>
+                <input
+                  type="text"
+                  size="35"
+                  id="contactSubject"
+                  name="contactSubject"
+                />
+              </div>
+
+              <div>
+                <label>
+                  Message <span className="required">*</span>
+                </label>
+                <textarea
+                  cols="50"
+                  rows="15"
+                  id="contactMessage"
+                  name="contactMessage"
+                ></textarea>
+              </div>
+
+              <div>
+                <button type="submit" onClick={sendEmail} className="submit">
+                  Submit
+                </button>
+                <span id="image-loader">
+                  <img alt="" src="images/loader.gif" />
+                </span>
+              </div>
+            </fieldset>
+          </form>
+
+          <div id="message-warning"> not sent try again</div>
+          <div id="message-success">
+            <i className="fa fa-check"></i>Your message was sent, thank you!
+            <br />
+          </div>
+        </div>
+
+        <aside className="four columns footer-widgets">
+          <div className="widget widget_contact">
+            <h4>Address and Phone</h4>
+            <p className="address">
+              {data?.name}
+              <br />
+              {data?.address.street} <br />
+              {data?.address.city}, {data?.address.state} {data?.address.zip}
+              <br />
+              <span>{data?.phone}</span>
+            </p>
+          </div>
+
+          <div className="widget widget_tweets"></div>
+        </aside>
+      </div>
+    </section>
+  );
+};
 
 export default Contact;
